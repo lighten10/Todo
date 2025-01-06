81d1f9e83fdc93d64bd5431d74eb53e3a0be0bd6
rahul joshi <31549264+lighten10@users.noreply.github.com>
2025-01-05 20:58:52 -0500

    Zod used in create and update todo

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
 const app = express();
+const {createTodo, updateTodo} = require('./types');
 
 app.use(express.json());
 
 //api to post todo
 app.post('todo' , (req,res) => {
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg : "You sent the wrong data!! please recheck"
+        })
+        return 
+    }
+    // connect to db and add todo
 
 });
 
@@ -14,5 +24,16 @@ app.get('todos' , (req,res) => {
 })
 
 app.put('completed' , (req,res) => {
+    const updatePayload = req.body
+    const parsePayload = updateTodo.safeParse(updatePayload)
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg : "You sent the worng data !! please check again"
+        })
+        return ;
+    }
+    //connect to Db and update todo
 
-}
+
+
+})
